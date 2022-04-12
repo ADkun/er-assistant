d884d2fe94bd2b508fe145f73012c656b736fd53
adkun <adgnn@foxmail.com>
2022-04-12 11:35:39 +0800

    pack

--- pack7z.sh
+++ pack7z.sh
@@ -2,4 +2,4 @@
 
 rm -f ../erassistant.7z
 cp -f main.go.exe 法环助手.exe
-7za a ../erassistant.7z 法环助手.exe tools/ mods/ bak/config.ini
+7za a ../erassistant.7z 法环助手.exe tools/ mods/ bak/config.ini README.md
--- pack7zpure.sh
+++ pack7zpure.sh
@@ -2,4 +2,4 @@
 
 rm -f ../erassistant_pure.7z
 cp -f main.go.exe 法环助手.exe
-7za a ../erassistant_pure.7z 法环助手.exe template
+7za a ../erassistant_pure.7z 法环助手.exe template README.md
