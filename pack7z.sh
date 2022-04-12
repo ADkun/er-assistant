289c6534a63b23fc35858d9a363fc5802d4ea7af
adkun <adgnn@foxmail.com>
2022-04-12 11:02:56 +0800

    打包工具

--- pack7z.sh
+++ pack7z.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 rm -f ../erassistant.7z
-mv main.go.exe 法环助手.exe
+cp -f main.go.exe 法环助手.exe
 7za a ../erassistant.7z 法环助手.exe tools/ mods/ bak/config.ini
---
+++ pack7zpure.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+rm -f ../erassistant_pure.7z
+cp -f main.go.exe 法环助手.exe
+7za a ../erassistant_pure.7z 法环助手.exe template
